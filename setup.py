99be0104760a9c5d5881df8a9f908e001b643093
semptrastar <alexeycacheknew@gmail.com>
2017-09-02 01:28:43 +0300

    FIx

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='instapi',
+setup(
+      name='instapi',
+      packages=['instapi'],
       version='0.1',
       description='Clean and simple Instagram API for Python 3.x',
       url='https://github.com/SemptraStar/Instapi',
       download_url = 'https://github.com/SemptraStar/instapi/archive/v._0.1.tar.gz',
       author='Alexey Sachek',
       author_email='olesksii.sachek@nure.ua',
-      packages=['instapi'])
+)
